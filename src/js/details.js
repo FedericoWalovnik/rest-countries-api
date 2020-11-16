b594e66bcb37c6a3563a28e1e4e38a82125088fe
Federico <fwalovnik@gmail.com>
2020-11-16 11:45:19 -0300

    handle click event on cards and redirecting to the details page

---
+++ src/js/details.js
@@ -0,0 +1,12 @@
+export const getAllCountries = async() =>{
+    const rawData = await fetch("https://restcountries.eu/rest/v2/all");
+    const convertedData = await rawData.json();
+    return convertedData;
+}
+
+
+export const getCountryByName = async(name) => {
+    const rawData = await fetch(`https://restcountries.eu/rest/v2/name/${name}`);
+    const convertedData = await rawData.json();
+    console.log(convertedData)
+}
