618b8f241606b1d00fa3349e7707b812ddb09603
Federico <fwalovnik@gmail.com>
2020-11-23 10:45:20 -0300

    backButton finished

--- src/js/getData.js
+++ src/js/getData.js
@@ -1,3 +1,6 @@
+export const localLink = "http://127.0.0.1:5500/src";
+export const prodLink = "";
+
 export const getAllCountries = async() =>{
     const rawData = await fetch("https://restcountries.eu/rest/v2/all");
     const convertedData = await rawData.json();
